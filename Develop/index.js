f0b396b09350da05db03898eb050f821121b60d3
Josh Covington <joshpcovington@gmail.com>
2023-11-18 15:04:49 -0700

    final code

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,78 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
+// Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your application:',
+    choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'Other'],
+  },
+  {
+    type: 'input',
+    name: 'githubUsername',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+];
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Function to write the README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('README.md has been generated!');
+    }
+  });
+}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to initialize the application
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    // Generate the markdown content with the answers
+    const markdown = generateMarkdown(answers);
 
-// TODO: Create a function to initialize app
-function init() {}
+    // Write the markdown content to a file
+    writeToFile('README.md', markdown);
+  });
+}
 
-// Function call to initialize app
+// Start the application
 init();
